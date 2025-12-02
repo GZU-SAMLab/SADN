b1ccf6897227b6b56412945c1650993ba4e63b49
SAMLab <147920838+GZU-SAMLab@users.noreply.github.com>
2025-12-02 20:02:55 +0800

    Create run_voc_fsod.sh

---
+++ run_voc_fsod.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+NET=$1
+NUNMGPU=$2
+EXPNAME=$3
+SPLIT_ID=$4
+
+SAVEDIR=workspace/SADN/voc-det/${EXPNAME}   #<-- change it to your path
+PRTRAINEDMODEL=pretrained_models/           #<-- change it to your path
+
+if [ "$NET"x = "r101"x ]; then
+  IMAGENET_PRETRAIN=${PRTRAINEDMODEL}/MSRA/R-101.pkl
+  IMAGENET_PRETRAIN_TORCH=${PRTRAINEDMODEL}/resnet101-5d3b4d8f.pth
+fi
+
+if [ "$NET"x = "r50"x ]; then
+  IMAGENET_PRETRAIN=${PRTRAINEDMODEL}/MSRA/R-50.pkl
+  IMAGENET_PRETRAIN_TORCH=${IMAGENET_PRETRAIN}/resnet50-19c8e357.pth
+fi
+
+
+# ------------------------------- Base Pre-train ---------------------------------- #
+python3 main.py --num-gpus ${NUNMGPU} --config-file configs/voc/dcfs_det_${NET}_base${SPLIT_ID}.yaml     \
+    --opts MODEL.WEIGHTS ${IMAGENET_PRETRAIN}                                                   \
+           OUTPUT_DIR ${SAVE_DIR}/sadn_det_${NET}_base${SPLIT_ID}
+
+# ----------------------------- Model Preparation --------------------------------- #
+python3 tools/model_surgery.py --dataset voc --method randinit                                \
+    --src-path ${SAVEDIR}/sadn_det_${NET}_base${SPLIT_ID}/model_final.pth                    \
+    --save-dir ${SAVEDIR}/sadn_det_${NET}_base${SPLIT_ID}
+BASE_WEIGHT=${SAVEDIR}/sadn_det_${NET}_base${SPLIT_ID}/model_reset_surgery.pth
+
+# ------------------------------ Novel Fine-tuning ------------------------------- #
+# --> 2. TFA-like, i.e. run seed0~9 10 times for gFSOD on voc
+classloss="DC" # "CE"
+for seed in 0 1 2 3 4 5 6 7 8 9
+do
+    for shot in 1 2 3 5 10 
+    do
+        TRAIN_ALL_NAME=voc_2007_trainval_all${SPLIT_ID}_${shot}shot_seed${seed}  
+        TEST_ALL_NAME=voc_2007_test_all${SPLIT_ID}
+        CONFIG_PATH=configs/voc/dcfs_gfsod_${NET}_novelx_${shot}shot_seedx.yaml
+
+        OUTPUT_DIR=${SAVEDIR}/sadn_gfsod_${NET}_novel${SPLIT_ID}/tfa-like-${classloss}/${shot}shot_seed${seed} 
+        python3 main.py --num-gpus ${NUNMGPU}  --config-file ${CONFIG_PATH}                            \
+            --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}                     \
+                   MODEL.ROI_BOX_HEAD.BBOX_CLS_LOSS_TYPE ${classloss} \
+                   DATASETS.TRAIN "('"${TRAIN_ALL_NAME}"',)" \
+                   DATASETS.TEST  "('"${TEST_ALL_NAME}"',)"  \
+                   TEST.CMCLIP_MODELPATH ${IMAGENET_PRETRAIN_TORCH} \
+                   TEST.CMCLIP_MODELTYPE $NET
+        #rm ${OUTPUT_DIR}/model_final.pth
+    done
+done
+# surmarize all results
+# python3 tools/extract_results.py --res-dir ${SAVE_DIR}/sadn_gfsod_${NET}_novel${SPLIT_ID}/tfa-like-${classloss} --shot-list 1 2 3 5 10  
